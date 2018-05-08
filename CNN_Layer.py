2941271bf71a9b0f5f25f83c7502b090d7ead28f
Peng Lu <lupeng@octal08.iro.umontreal.ca>
2018-05-08 13:52:18 -0400

    add cnn layer for sequence encoding

--- CNN_Layer.py
+++ CNN_Layer.py
@@ -4,20 +4,26 @@ import torch.nn.init as init
 import torch.nn.functional as F
 from torch.autograd import Variable
 from WeightNorm import WeightNormConv2d
+import math
 
 SCALE_WEIGHT = 0.5 ** 0.5
 
 def shape_transform(x):
     """ Tranform the size of the tensors to fit for conv input. """
     return torch.unsqueeze(torch.transpose(x, 1, 2), 3)
+def reshape_transform(x):
+    return torch.squeeze(torch.transpose(x, 1, 2), 3)
 
 
 class GatedConv(nn.Module):
-    def __init__(self, input_size, width=3, dropout=0.2, nopad=False):
+    def __init__(self, input_size, width=3, dropout=0.2, dilation=1, nopad=False):
         super(GatedConv, self).__init__()
-        self.conv = WeightNormConv2d(input_size, 2 * input_size,
-                                     kernel_size=(width, 1), stride=(1, 1),
-                                     padding=(width // 2 * (1 - nopad), 0))
+        self.conv = WeightNormConv2d(input_size, 
+                                     2 * input_size,
+                                     kernel_size=(width, 1), 
+                                     stride=(1, 1),
+                                     padding=(width // 2 * (1 - nopad)*dilation, 0), 
+                                     dilation=dilation)
         init.xavier_uniform(self.conv.weight, gain=(4 * (1 - dropout))**0.5)
         self.dropout = nn.Dropout(dropout)
 
@@ -31,17 +37,30 @@ class GatedConv(nn.Module):
 
 class StackedCNN(nn.Module):
     def __init__(self, num_layers, input_size, cnn_kernel_width=3,
-                 dropout=0.2):
+                 dropout=0.2, all_layers_hid=False):
         super(StackedCNN, self).__init__()
         self.dropout = dropout
         self.num_layers = num_layers
         self.layers = nn.ModuleList()
+        self.all_layers_hid = all_layers_hid
         for i in range(num_layers):
             self.layers.append(
-                GatedConv(input_size, cnn_kernel_width, dropout))
+                GatedConv(input_size, cnn_kernel_width, dropout, 2**i))
 
     def forward(self, x, hidden=None):
+        if self.all_layers_hid:
+            hid = []
         for conv in self.layers:
             x = x + conv(x)
             x *= SCALE_WEIGHT
-        return x
+            if self.all_layers_hid:
+                hid.append(x)
+        return x if not self.all_layers_hid else hid
+def test():
+    inputs = shape_transform(Variable(torch.randn(10, 50, 10)))
+    model = StackedCNN(3, 10)
+    out = model(inputs)
+    print(out.size())
+
+if __name__ == '__main__':
+    test()
